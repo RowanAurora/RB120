ac31d2545930f21bee8997e1ce531195eac19c13
rowan aurora <rowanivordraws@gmail.com>
2022-12-26 14:49:21 -0500

    Finishings

--- lesson_2/oorps/rps_2.rb
+++ lesson_2/oorps/rps_2.rb
@@ -1,4 +1,7 @@
-require 'pry'
+require 'yaml'
+LANGUAGE = 'en'
+MESSAGES = YAML.load_file('rps.yml')
+
 module Formatable
   def to_s
     self.class.to_s.split("::")[-1]
@@ -7,71 +10,67 @@ end
 
 module Displayable
   VALID = ['y', 'yes', 'n', 'no']
-  ROUNDS = 5
-  ANIMATE_TEXT =["ROCK", "PAPER", "SCISSORS", "LIZARD", "SPOCK", "GO!"]
-  
-  def word_line_boogie
-    counter = 0
-    
-    6.times do 
-      sleep 0.3
+  ROUNDS = 3
+  ANIMATE_TEXT = ["ROCK", "PAPER", "SCISSORS", "LIZARD", "SPOCK", "GO!"]
+  LOSE_GRAPHIC = [%( .  _.-^^---....,,--_n ), %( _--                  L--_),
+                  %(<                        >), %(|                         |),
+                  %( L._                   _./), %(    ```--. . , ; .--''' ),
+                  %(          | |   |        ), %(       .-=||  | |=-.   ),
+                  %(       `-=#$%&%$#=-'   ), %(          | ;  :|     ),
+                  %( _____.,-#%&$@%#&#~,._____)]
+
+  def messages(message, lang='en')
+    MESSAGES[lang][message]
+  end
+
+  def prompt(key)
+    message = messages(key, LANGUAGE)
+    puts message
+  end
+
+  def text_animation
+    0.upto(5) do |i|
+      sleep 0.2
       print "\r"
-      print sprintf("%20s", ANIMATE_TEXT[counter])
-      counter += 1
-      # case counter
-      # when 0
-      #   counter += 1
-      #   print "        ROCK       "
-      # when 1
-      #   counter += 1
-      #   print "       PAPER       "
-      # when 2
-      #   counter += 1
-      #   print "      SCISSORS     "
-      # when 3
-      #   counter += 1
-      #   print "       LIZARD      "
-      # when 4
-      #   counter += 1
-      #   print "       SPOCK       "
-      # when 5  
-      #   counter += 1
-      #   print "        GO!          "
-      #   sleep 0.5
-      # end
+      print format("%20s", ANIMATE_TEXT[i])
     end
     sleep 0.5
     puts
   end
 
   def display_welcome_message
-    puts 
-    puts "----------------ROCK  PAPER  SCISSORS  LIZARD  SPOCK------------------"
-    puts "█▀█ █ █▀ █▀▀   █▀█ █▀▀   ▀█▀ █░█ █▀▀   █▀▄▀█ ▄▀█ █▀▀ █░█ █ █▄░█ █▀▀ █▀"
-    puts "█▀▄ █ ▄█ ██▄   █▄█ █▀░   ░█░ █▀█ ██▄   █░▀░█ █▀█ █▄▄ █▀█ █ █░▀█ ██▄ ▄█"
+    puts
+    prompt "welcome"
+    prompt "title_one"
+    prompt "title_two"
     puts
   end
-  
-  def display_rules 
-    puts "Would You like to see the rules?(y/n)"
+
+  def rules_set
+    prompt "rule1"
+    prompt "rule2"
+    puts
+    prompt "rule3"
+    prompt "rule4"
+    puts "Get #{ROUNDS} wins before the computer to stop the apocalypse"
+    puts
+  end
+
+  def display_rules
+    prompt "rules"
     answer = nil
     loop do
       answer = gets.chomp.downcase
+      puts
       break if VALID.include?(answer)
-      puts "Please input yes or no"
+      prompt "yesno"
     end
-    if answer.start_with?('y')
-      puts "Scissors cuts Paper covers Rock crushes Lizard poisons Spock smashes Scissors "
-      puts "decapitates Lizard eats Paper disproves Spock vaporizes Rock crushes Scissors"
-      puts "First, pick your name. Then, choose your opponant."
-      puts "Enter your move by typing one of the choices Rock, Paper, Scissors, Lizard, Spock"
-      puts "Get #{ROUNDS} wins before the computer to stop the apocalypse"
-    end
+    rules_set if answer.start_with?('y')
   end
 
   def display_goodbye_message
     puts
-    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock! Bye!"
+    prompt "bye"
   end
 
   def move_display
@@ -80,38 +79,28 @@ module Displayable
     puts "#{computer.name} chose #{computer.move}"
   end
 
-  def bomb 
-    puts %( .  _.-^^---....,,--_n )       
-    puts %( _--                  L--_)  
-    puts %(<                        >)
-    puts %(|                         |) 
-    puts %( L._                   _./)  
-    puts %(    ```--. . , ; .--''' )      
-    puts %(          | |   |        )     
-    puts %(       .-=||  | |=-.   )
-    puts %(       `-=#$%&%$#=-'   )
-    puts %(          | ;  :|     )
-    puts %( _____.,-#%&$@%#&#~,._____)
+  def bomb
+    LOSE_GRAPHIC.each { |line| puts line }
   end
-  
+
   def display_winner
     if human_won?
       puts "#{human.name} won this battle!"
     elsif computer_won?
       puts "#{computer.name} defeated you!"
     else
-      puts "It's a tie!"
+      prompt "tie"
     end
   end
 
   def hero_display
     if human.score == ROUNDS
-      puts "You saved the world! Now the terminators are coming for you."
+      prompt "win"
     elsif computer.score == ROUNDS
       bomb()
-      puts "You failed to save the world! Fight for you new overlord's amusement."
-      human.name = "Wastelander"
-      computer.name = "SUPREME OVERLORD"
+      prompt "lose"
+      human.name = messages "hero"
+      computer.name = messages "lord"
     end
   end
 
@@ -121,15 +110,15 @@ module Displayable
   end
 
   def move_history_display
-    puts 'Would you like to see the move history?(y/n)'
+    prompt "move_history"
     answer = nil
-    loop do 
+    loop do
       answer = gets.chomp.downcase
       puts
       break if VALID.include?(answer)
-      puts "Please input yes or no"
+      prompt "yesno"
     end
-      puts @move_history if answer.start_with?('y') 
+    puts @move_history if answer.start_with?('y')
   end
 end
 
@@ -140,39 +129,34 @@ module Moves
     DANGER = ['scissors', 'lizard', 'rock']
 
     def >(other_move)
-     self.class::WINS.include?(other_move.to_s.downcase)
+      self.class::WINS.include?(other_move.to_s.downcase)
     end
   end
 
   class Rock < Move
     WINS = ['scissors', 'lizard']
-    LOSES = ['paper', 'spock']
   end
 
   class Paper < Move
     WINS = ['rock', 'spock']
-    LOSES = ['scissors', 'lizard']
   end
 
   class Scissors < Move
     WINS = ['paper', 'lizard']
-    LOSES = ['rock', 'spock']
   end
 
   class Lizard < Move
     WINS = ['spock', 'paper']
-    LOSES = ['rock', 'scissors']
   end
 
   class Spock < Move
     WINS = ['scissors', 'rock']
-    LOSES = ['lizard', 'paper']
   end
 end
 
-module Players 
-
+module Players
   class Player
+    include Displayable
     attr_accessor :move, :name, :score
 
     MOVE_INPUT = {
@@ -190,13 +174,14 @@ module Players
 
   class Human < Player
     attr_accessor :choice
+
     def set_name
       n = nil
       loop do
-        puts "What's your name?"
+        prompt "name"
         n = gets.chomp
         break unless n.empty?
-        puts "Sorry, must enter a name"
+        prompt "sorry_name"
       end
       self.name = n
     end
@@ -205,10 +190,10 @@ module Players
       choice = nil
       loop do
         puts
-        puts "Pick : Rock, Paper, Scissors, Lizard or Spock?"
+        prompt "pick"
         choice = gets.chomp.downcase
         break if MOVE_INPUT.keys.include? choice
-        puts "Sorry, invalid input"
+        prompt "not_valid"
       end
       @@choice = choice
       self.move = MOVE_INPUT[choice]
@@ -224,9 +209,8 @@ module Players
     include Formatable
 
     def set_name
-      self.name = self.to_s
+      self.name = to_s
     end
-
   end
 
   class Printer < Computer
@@ -242,21 +226,21 @@ module Players
   end
 
   class Hades < Computer
-    def choose 
+    def choose
       self.move = MOVE_INPUT[Moves::Move::DANGER.sample]
     end
   end
 
   class Watson < Computer
-  LOOSERS = {
-    'rock' => ['paper', 'spock'],
-    'paper' => ['scissors', 'lizard'],
-    'scissors' => ['spock', 'rock'],
-    'lizard' => ['rock', 'scissors'],
-    'spock' => ['lizard', 'paper']
-  }
+    LOOSERS = {
+      'rock' => ['paper', 'spock'],
+      'paper' => ['scissors', 'lizard'],
+      'scissors' => ['spock', 'rock'],
+      'lizard' => ['rock', 'scissors'],
+      'spock' => ['lizard', 'paper']
+    }
 
-    def choose 
+    def choose
       self.move = MOVE_INPUT[(LOOSERS[Human.human_move].sample)]
     end
   end
@@ -264,15 +248,17 @@ end
 
 module Movestorable
   def movekeep
-    @move_history << ["#{human.name}: #{human.move}", 
-      "#{computer.name}: #{computer.move}", '-----------']
+    @move_history << ["#{human.name}: #{human.move}",
+                      "#{computer.name}: #{computer.move}", '-----------']
   end
 end
 
 class RPSGame
   attr_accessor :human, :computer
+
   include Displayable
   include Movestorable
+
   def initialize
     display_welcome_message
     display_rules
@@ -285,30 +271,26 @@ class RPSGame
     n = nil
     loop do
       puts
-      puts "Pick your opponant! Edi, Hades, Watson or Printer"
+      prompt "enemy"
       n = gets.chomp.capitalize
       break if Players::Computer::NAMES.include?(n)
-      puts "Sorry, must enter opponents name"
+      prompt "valid_enemy"
     end
     n
   end
 
   def enemy
     n = pick_enemy
-    case n 
-    when 'Edi'
-      Players::Edi.new
-    when 'Hades'
-      Players::Hades.new
-    when 'Watson'
-      Players::Watson.new
-    when 'Printer'
-      Players::Printer.new
+    case n
+    when 'Edi' then Players::Edi.new
+    when 'Hades' then Players::Hades.new
+    when 'Watson' then Players::Watson.new
+    when 'Printer' then Players::Printer.new
     end
   end
 
   def human_won?
-     human.move > computer.move
+    human.move > computer.move
   end
 
   def computer_won?
@@ -318,7 +300,7 @@ class RPSGame
   def score_update
     if human_won?
       human.score += 1
-    elsif computer_won? 
+    elsif computer_won?
       computer.score += 1
     end
   end
@@ -326,29 +308,36 @@ class RPSGame
   def play_again?
     answer = nil
     loop do
-      puts "Would you like to play again?(y/n)"
-      answer = gets.chomp
-      break if ['y', 'n'].include? answer.downcase
-      puts "sorry, must be y or n"
+      prompt "again"
+      answer = gets.chomp.downcase
+      break if ['yes', 'y', 'no', 'n'].include?(answer)
+      prompt "yesno"
     end
-    answer == 'y'
+    answer.start_with?('y')
+  end
+
+  def move_section
+    human.choose
+    computer.choose
+    movekeep
+  end
+
+  def display_section
+    text_animation
+    move_display
+    puts
+    score_update
+    display_winner
+    hero_display
+    puts
+    score_display
+    puts
   end
 
   def play
-    
     loop do
-      human.choose
-      computer.choose
-      movekeep
-      word_line_boogie
-      move_display
-      puts 
-      score_update
-      display_winner
-      hero_display
-      puts 
-      score_display
-      puts
+      move_section
+      display_section
       break unless play_again?
     end
     move_history_display
